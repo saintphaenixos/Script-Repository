3dad0994e6262cace2311546b72b6ad5ae0a9c8b
Kent <Kduback@pima.edu>
2021-01-08 09:35:23 -0700

    Ventoy Drive maker version change:
    Ventoy Drive maker version change, as well as change of execution permissions for JoinAD.sh

--- Linux scripts/Old/ventoydriveupdater1.0.sh
+++ Linux scripts/Old/ventoydriveupdater1.0.sh
@@ -5,7 +5,6 @@ USR=kduback
 
 #Last we make a temp file of all mounted drives under /Media/$USR and order rsync to over-write all files on those partitions with everything in Ventoy.Export on my machine. Then we remove the temp list.
 ls /media/$USR | grep SCCM > mountlist.temp
-#cat mountlist.temp | time parallel -j+0 --progress "cp -R /home/$USR/Downloads/Ventoy.Export/* /media/$USR/{}/"
 cat mountlist.temp | time parallel -j+0 --progress "rsync --info=progress2 -r /home/$USR/Downloads/Ventoy.Export/* /media/$USR/{}/"
 #xargs -a mountlist.temp -I{} -d'\n' cp -R /home/kduback/Downloads/Ventoy.Export/* /media/kduback/{}/
 rm mountlist.temp
