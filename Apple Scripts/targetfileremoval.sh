fda42031fe0407331f7518d894240b07e7c318ab
Kent <Kduback@pima.edu>
2021-02-17 12:44:41 -0700

    Renamed Adobe removals to be more general:
    script removes target file on computer as given in Parameter 4 in JAMF, as root user. This concept is the same for the uninstaller, which can be given a parameter in JAMF to attempt an uninstall, but care must be taken to know if additional flags are needed for silent running.

---
+++ Apple Scripts/targetfileremoval.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#!/bin/zsh
+
+#A simple script to remove target files\folders\or programs from a Unix/Linux machine.
+
+#First lets check if the user is Root or not.
+if [ "$UID" -gt 0 ]; then
+     red "This script must be run as root!"
+     echo "exiting..."
+     exit
+fi
+
+#Now we have our logic to remove the target file, folder, or program.
+
+if [ -f "${4}" ];
+  then
+    echo "Target file, folder, or program is present, attempting removal."
+  else
+    echo "exiting... as Program/File is not present."
+    exit 0
+fi
+
+rm -rf ${4}
---
+++ Apple Scripts/targetuninstall.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#!/bin/zsh
+
+#A simple script to attempt an uninstall of target program from a Macintosh machine.
+#Ensure that targeted Uninstaller can be run without arguments, or if arguments or flags are needed to add them to the additional jamf lines
+
+#First lets check if the user is Root or not.
+if [ "$UID" -gt 0 ]; then
+     red "This script must be run as root!"
+     echo "exiting..."
+     exit
+fi
+
+#Now we have our logic to attempt uninstall of the target program.
+
+if [ -f "${4}" ];
+  then
+    echo "Uninstaller is present, attempting uninstall."
+  else
+    echo "exiting... as uninstaller is not present."
+    exit 0
+fi
+
+sudo ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11}
