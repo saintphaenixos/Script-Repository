f2df1e41f8e4718793e436d88653e1cd1dcff292
Kent <Kduback@pima.edu>
2021-02-17 09:55:34 -0700

    AcrobatUninstall2.0.sh improved:
    Added and improved Acrobat Uninstall 2.0
    
    last minor changes with findanddeletegui.sh

---
+++ Apple Scripts/AcrobatUninstall2.0.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Improved by Kent DuBack II 2.17.2021, idea by Will Crabtree.
+
+if [ -f "/Applications/Adobe\ Acrobat\ DC/Adobe\ Acrobat.app/Contents/Helpers/Acrobat\ Uninstaller.app" ];
+  then
+    echo "Uninstaller is present, attempting run."
+  else
+    echo "exiting... as uninstaller is not present."
+    exit 0
+fi
+
+# Remove Adobe Acrobat DC
+
+#sudo /Applications/Adobe\ Acrobat\ DC/Adobe\ Acrobat.app/Contents/Helpers/Acrobat\ Uninstaller.app/Contents/Library/LaunchServices/com.adobe.Acrobat.RemoverTool Uninstall /Applications/Adobe\ Acrobat\ DC/Adobe\ Acrobat.app
