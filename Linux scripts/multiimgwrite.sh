3e2b091a16d7b1f61e477fef1aca76430e60792b
Kent <Kduback@pima.edu>
2021-03-11 07:27:47 -0700

    multiimgwrite.sh Written:
    wrote a script to write multiple .img files to multiple target drives using zenity for GUI dialogues as needed. Minor changes to organization and archival of other scripts as well.

--- Linux scripts/multiimgwrite.sh
+++ Linux scripts/multiimgwrite.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 #!/bin/zsh
 
+#This is a script to write one image to multiple disks selected through Zenity, using the dcfldd tool, it only supports .img files, but may be changed in the future.
+
 #Lets check if you are root first, as you need to be root for this:
 [ "$UID" -gt 0 ] && echo -e "This script must be run as root! \n exiting..." && exit 1
 
-#first lets ensure that needed software for this is installed:
-#Smartmontools is needed, but doesn't have an attached binary, so this may not catch it.
-for program in perl zenity; do
+#We'll go ahead and check if these programs are installed, and get them installed.
+for program in dcfldd zenity; do
   installed=$(command -v $program)
   [[ -z "$installed" ]] && echo "$program is not installed" && sudo apt update && sudo apt install $program || echo "$program is installed"
 done
 
-#Lets check for smartmontools seperately as they are a metapackage:
-smartmoninstalled=$(command -v smartctl)
-[[ -z "$smartmontinstalled" ]] && echo "smartmontools is not installed" && sudo apt update && sudo apt install smartmontools || echo "smartmontools is installed"
+# First lets select the img file we wish to work with:
+imglocation=$(zenity --file-selection --filename="/home" --title="Select a File")
 
 #Lets create a function to run to get a list of drives, output it to GUI and then use it with the intended command:
-
 Outputdrivesviagui () (
 
   #Lets create a sub-function for listing the names of the drives using PCRE2 in Perl.
@@ -32,11 +31,21 @@ Outputdrivesviagui () (
   [ "$?" -gt 0 ] && exit 1
 
   #Lastly we add /dev/ to the names of the drives selected through Zenity.
-  preparedlist=$(echo "$drivelist" | awk '{print "/dev/" $0}')
+  preparedlist=$(echo "$drivelist" | awk '{print "of=/dev/" $0}')
   echo "$preparedlist"
 )
 
-driveselection=$(Outputdrivesviagui)
-echo -e "you selected these drives: \n$driveselection"
+targetslist=$(Outputdrivesviagui)
+modtargets=$(echo "$targetslist" | tr "\n" " " )
+argtargs=$(echo "${modtargets%?}")
 
-echo "$driveselection" | xargs -I{} smartctl {} -a | grep 9\ Power
+#Lets do a sanity check to make sure we don't do this on accident:
+confirmation=$(zenity --entry --text "Please confirm that these drives are correct: $targetslist by typing the word "confirm"")
+#if the Zenity window is closed or cancelled, then it will kill the script:
+[ "$?" -gt 0 ] && exit 1
+
+#Now lets check to see if confirmation
+[ "$confirmation" != "confirm" ] && zenity --warning --text "confirm not entered, or entered incorrectly, cancelling." && exit 1
+
+# Now we'll go ahead and utilize the list we have generated with our function and get the images burnt to the new mediums.
+echo "dcfldd if=\"$imglocation\" $argtargs bs=1M"
