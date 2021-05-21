9d7671b4f3d5228407be8002df37ed461610abe9
Kent <Kduback@pima.edu>
2021-05-21 15:39:11 -0700

    Added Newcomputer.sh and .programs:
    Added a script to install all the needed programs that I want on a new machine, and a modifiable list that I can edit with new programs as I need them.

---
+++ Linux scripts/NewComputerProgInstall.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#Lets make sure that we are running as root before we start:
+[ "$UID" -gt 0 ] && echo -e "This script must be run as root! \n exiting..." && exit 1
+
+scriptname=$(basename -- "$0")
+[ -z "ps -p $$ | grep bash" ] && echo -e "This Script must be run with bash ./$scriptname" && exit 1
+
+#Lets go ahead and create an array of all the programs we'll want installed, we'll do this from a file:
+Programs=$(<./newcomputer.programs)
+
+#lastly lets update the apps list so it doesn't have to be done repeatedly:
+sudo apt update
+
+#Now lets install everything.
+for program in $Programs ; do
+  installed=$(command -v $program)
+  [[ -z "$installed" ]] && echo "$program is not installed" && sudo apt install $program || echo "$program is installed"
+done
+
+###################################################################
+
+#Now lets do some other things, like install oh-my-zsh:
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#Now lets download my favorite theme for it: Oh-My-Via:
+git clone https://github.com/badouralix/oh-my-via.git $ZSH/custom/themes/oh-my-via
