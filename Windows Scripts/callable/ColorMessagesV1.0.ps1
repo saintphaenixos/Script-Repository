6a5befb2ee962186cfe56d3940fb24da7889e446
Kent <Kduback@pima.edu>
2021-11-01 16:07:14 -0700

    Small changes to formatting in scripts:
    small changes to formatting, and adding executable bit to GUIX installers.

--- Windows Scripts/callable/ColorMessagesV1.0.ps1
+++ Windows Scripts/callable/ColorMessagesV1.0.ps1
@@ -45,6 +45,7 @@ function Write-Color() {
 #=========================================
 #This is just a small trick to show all the color combinations at once.
 #=========================================
+
 <#>
 $colors = [enum]::GetValues([System.ConsoleColor]) | Select-Object @{N='ColorObject';E={$_}}, @{N='ColorName'; E={ If ($_.ToString().substring(0,3) -eq 'Dar' ){ $_.ToString().Substring(4) + 'DARK' } else { $_.ToString() } } } | Sort-Object Colorname ; Foreach ($bgcolor in $colors.ColorObject){ Foreach ($fgcolor in $colors.ColorObject) { Write-Host "$fgcolor|" -ForegroundColor $fgcolor -BackgroundColor $bgcolor -NoNewLine } Write-Host " on $bgcolor" }
 </#>
