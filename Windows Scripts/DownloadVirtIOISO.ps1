08a18a33160fdf0fc1eb2d59f1bb3d18401693c0
Kent <Kduback@pima.edu>
2021-11-01 17:10:42 -0700

    Wrote Minor Script: DownloadVirtIOISO.ps1
    Allows a user to find and download the VirtIO drivers for Linux Windows Virtualization.

---
+++ Windows Scripts/DownloadVirtIOISO.ps1
@@ -0,0 +1,10 @@
+#This is a simple script to reach out and download the latest stable VirtIO drivers for Windows to operate in QEMU/KVM for my purposes.
+
+#Written for Pima Community College by Kent DuBack II 11.1.2021
+
+#Change directory and download the .iso
+cd ~\Dowloads
+Start-BitsTransfer https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso
+
+#Mount the ISO and begin installation:
+Mount-DiskImage -ImagePath virtio-win.iso
