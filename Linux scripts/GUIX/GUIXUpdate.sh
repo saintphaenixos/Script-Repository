b3da0e49c01495a747cd4097fa69ccbe626dab34
fenix <Daystar999964@gmail.com>
2021-11-03 20:18:31 -0700

    Minor edits in GUIX Scripts:
    adding more garbage collection.

--- Linux scripts/GUIX/GUIXUpdate.sh
+++ Linux scripts/GUIX/GUIXUpdate.sh
@@ -2,12 +2,18 @@
 
 #This is a script to update GUIX in place on a machine that has already had it installed.
 
+#DO NOT RUN THIS SCRIPT AS ROOT! LETS ENSURE THAT HERE:
+[ "$UID" == 0 ] && echo -e "This script cannot be run as root! \n exiting..." && exit 1
+
 #Update GUIX:
 guix pull
 sudo -i guix pull
 
 #Run the garbage collector:
 guix gc
+
+#Remove the older generations:
+guix gc --delete-generations
 
 #add the needed paths:
 export PATH="$HOME/.config/guix/current/bin:$PATH"
