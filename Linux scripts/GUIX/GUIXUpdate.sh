d31c14f96ed6f68b45913e295f25901ca9177a0a
Kent <Kduback@pima.edu>
2021-11-15 17:55:49 -0700

    changed execution environments of two scripts:
    No need for it to be zsh in these cases.

--- Linux scripts/GUIX/GUIXUpdate.sh
+++ Linux scripts/GUIX/GUIXUpdate.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 #This is a script to update GUIX in place on a machine that has already had it installed.
 
--- Linux scripts/GUIX/RemoveGUIX.sh
+++ Linux scripts/GUIX/RemoveGUIX.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 #A simple script to completely remove GUIX from a non GUIX-SD distribution.
 
