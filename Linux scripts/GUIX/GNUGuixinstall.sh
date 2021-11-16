5d4d5b3c916ab8bbae9094ffabc6341bdcdf8aeb
Kent <Kduback@pima.edu>
2021-11-15 17:37:29 -0700

    GNUGuixinstall.sh More needed programs added:
    added more programs for installation in GNUGuixInstall.sh.

--- Linux scripts/GUIX/GNUGuixinstall.sh
+++ Linux scripts/GUIX/GNUGuixinstall.sh
@@ -8,11 +8,14 @@
 git=$(command -v git)
 [[ -z "$git" ]] && echo "Git is not installed, please install that before running this shell script."
 
+dosu=$(command -v sudo)
+[[ -z "$dosu" ]] && echo "Git is not installed, please install that and configure it before running this shell script."
+
 #Now lets install the required software for this script:
 # The required software for this is: Curl, zsh, and wget, lets check for them and get them installed.
-for program in wget; do
+for program in wget xz-utils gpg; do
   installed=$(command -v $program)
-  [[ -z "$installed" ]] && echo "$program is not installed, Please install $program from a seperate elevated Command prompt, and then run this script again." && exit 1 || echo "$program is installed"
+  [[ -z "$installed" ]] && echo "$program is not installed" && sudo apt update && sudo apt install $program || echo "$program is installed"
 done
 
 #Lets set a color function:
