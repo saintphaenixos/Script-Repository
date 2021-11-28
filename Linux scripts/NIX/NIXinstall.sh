dc2cfa4255fc3513dd88ca30f5ef6cc7673657b4
Fenix <daystar999964@gmail.com>
2021-11-28 09:02:18 -0700

    Fixed variables, and added execute bit:

--- Linux scripts/NIX/NIXinstall.sh
+++ Linux scripts/NIX/NIXinstall.sh
@@ -3,8 +3,8 @@
 #This is a script to check for and download the requirements for the NIX packaging system to a Debian Based OS. It will be tested on both Debian 11, and Ubuntu 21.10.
 
 #Lets set some variables:
-currentuser = $USER
-sudoexist = $(grep $currentuser < /etc/sudoers)
+currentuser=$USER
+sudoexist=$(grep $currentuser < /etc/sudoers)
 
 #First lets get prepared by installing sudo and curl, and checking if sudo is already installed or not.
 if [[ -z "$sudoexist" ]]
@@ -14,7 +14,7 @@ then #First lets get our requirements installed:
     [[ -z "$installed" ]] && echo "$program is not installed" && sudo apt update && sudo apt install $program || echo "$program is installed"
   done
 else
-  echo "Input password for root." && sleep 3
+  echo "Input password for root." && sleep 1
   su root
   for program in sudo curl; do
     installed=$(command -v $program)
@@ -22,7 +22,7 @@ else
   done
 echo "Input password for your standard user account, you started with."
 su $currentuser
-echo "Now add yourself to the sudoers file." && sleep 3
+echo "Now add yourself to the sudoers file." && sleep 1
 visudo
 fi
 
