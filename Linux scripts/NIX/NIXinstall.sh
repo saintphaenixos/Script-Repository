46d4067651790761f8322df6200625c75de2c311
Fenix <daystar999964@gmail.com>
2021-11-28 09:16:56 -0700

    Simplified Script:
    It was doing too much and wouldn't work the way I had originally intended.

--- Linux scripts/NIX/NIXinstall.sh
+++ Linux scripts/NIX/NIXinstall.sh
@@ -2,29 +2,18 @@
 
 #This is a script to check for and download the requirements for the NIX packaging system to a Debian Based OS. It will be tested on both Debian 11, and Ubuntu 21.10.
 
-#Lets set some variables:
-currentuser=$USER
-sudoexist=$(grep $currentuser < /etc/sudoers)
+#Lets set some variables and a color function:
+sudoexist=$(grep $USER < /etc/sudoers)
+fuchsia() {
+  echo -e "\e[38;5;13m${@}\e[0m"
+}
 
 #First lets get prepared by installing sudo and curl, and checking if sudo is already installed or not.
-if [[ -z "$sudoexist" ]]
-then #First lets get our requirements installed:
-echo "Input password for root." && sleep 1
-su root
-  for program in sudo curl; do
-    installed=$(command -v $program)
-    [[ -z "$installed" ]] && echo "$program is not installed" && apt update && apt install $program || echo "$program is installed"
-  done
-else
-  for program in curl; do
-    installed=$(command -v $program)
-    [[ -z "$installed" ]] && echo "$program is not installed" && sudo apt update && sudo apt install $program || echo "$program is installed"
-  done
-echo "Input password for your standard user account, you started with."
-su $currentuser
-echo "Now add yourself to the sudoers file." && sleep 1
-visudo
-fi
+[[ -z "$sudoexist" ]] && fuschia "Sudo doesn't exist on this system, please install and configure it first."
+for program in curl; do
+  installed=$(command -v $program)
+  [[ -z "$installed" ]] && echo "$program is not installed" && sudo apt update && sudo apt install $program || echo "$program is installed"
+done
 
 #Lastly lets go ahead and install Nix:
 curl -L https://nixos.org/nix/install | sh
