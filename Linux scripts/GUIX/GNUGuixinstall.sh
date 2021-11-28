6b3b4f3ee41f1b0054585f3b03cdf3c106aaa723
Fenix <daystar999964@gmail.com>
2021-11-28 11:33:18 -0700

    Archiving Older Scripts:
    More work on GUIX and NIX:

--- Linux scripts/GUIX/GNUGuixinstall.sh
+++ Linux scripts/GUIX/GNUGuixinstall.sh
@@ -17,11 +17,7 @@ done
 
 #Lets set a color function:
 teal() {
-    if (( $# == 0 )) ; then
-        xargs -r -I{} echo -e "\e[38;5;6m"{}"\e[0m" < /dev/stdin
-    else
         echo -e "\e[38;5;6m${@}\e[0m"
-    fi
 }
 
 #Now lets gather the current Guix install script for use here, we'll put it into the /tmp location.
