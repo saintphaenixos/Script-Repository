20e62c46a32bafafc8720c91fa7afe0fe050f065
Craby4GitHub <github@willcrabtree.com>
2023-03-14 16:30:08 -0700

    Comment and Readability Update
    Removed "bad" comments and replaced with useful comments.
    Renamed a few variables.
    Update configuration cycle array to hash table for readability.
    Other general readability changes.

--- Windows Scripts/SCCMClientInventories v3.2.ps1
+++ Windows Scripts/SCCMClientInventories v3.2.ps1
@@ -1,7 +1,8 @@
-# This script automatically runs the SCCM task sequences 500 times every 20 Minutes.
-# It was originally written by Kent DuBack II on 12/5/2019 for Pima Community College.
-
+# Originally written by Kent DuBack II on 12/5/2019 for Pima Community College.
 # With many additions by Craby4Github on occasion
+
+# Summary: This script performs system checks, enforces naming conventions, runs a GPUpdate,
+# connects to Wi-Fi, checks for pending reboots, and iteratively runs Configuration Manager Cycles.
 
 # Relaunch as an elevated process:
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
@@ -9,17 +10,12 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
   exit
 }
 
-######################################################################
-# Computer Name Check, Gpupdate and Variables/Arrays
-######################################################################
-
-#Lets verify the computer name matches our naming conventions
+# Get the computer system information.
 $computername = Get-WmiObject Win32_ComputerSystem
 
+# Validate the computer name against the naming conventions and display an error message if the name does not match the conventions.
 if ($computername.Name -notmatch "^(?<Campus>([a-z]{2,3}|\d{2}))(?<Dash>-?)(?<BuildingandRoom>([a-z]{1,2}\d{2,3})|[a-z]{3})(?<PCCNumber>\d{6})(?<Suffix>[a-z]{2}|[v]\d)$") {
-
   Add-Type -AssemblyName System.Windows.Forms | Out-Null
-
   [System.Windows.Forms.MessageBox]::Show(
     "Computer name does not match naming conventions.`n
     Please rename the computer.`n
@@ -27,13 +23,11 @@ if ($computername.Name -notmatch "^(?<Campus>([a-z]{2,3}|\d{2}))(?<Dash>-?)(?<Bu
   )
 }
 
-#Here is the title and all data afterword tells us if it's functioning.
+# Force a Group Policy update
 Write-Host "==== Starting GPUpdate ====" -ForegroundColor Red -BackgroundColor White
-
-#Before we get serious, lets run a GPUpdate:
 gpupdate /force
 
-#Connect to WiFi if there is a wireless network adapter
+# Look for a Wi-Fi adapter and if found, connect to the PimaDot1x Wi-Fi network.
 $wirelesNetworkAdapters = Get-NetAdapter -Physical -name "Wi*Fi*"
 if ($null -ne $wirelesNetworkAdapters) {
   Write-Host "==== Connect to PimaDot1x ====" -ForegroundColor Red -BackgroundColor White
@@ -42,41 +36,35 @@ if ($null -ne $wirelesNetworkAdapters) {
   Start-Sleep -Seconds 5
 }
 
-#Alright lets list the cycles in an array and write out to the terminal that we are starting the cycles
-Write-Host "==== Starting Configuration Manager Cycles ====" -ForegroundColor Red -BackgroundColor White
-$ConfigManCycles = @(
-    ('{00000000-0000-0000-0000-000000000121}', 'Application Deployment Evaluation Cycle'),
-    ('{00000000-0000-0000-0000-000000000003}', 'Discovery Data Collection Cycle'),
-    ('{00000000-0000-0000-0000-000000000010}', 'File Collection Cycle'),
-    ('{00000000-0000-0000-0000-000000000001}', 'Hardware Inventory Cycle'),
-    ('{00000000-0000-0000-0000-000000000021}', 'Machine Policy Retrieval Cycle'),
-    ('{00000000-0000-0000-0000-000000000022}', 'Machine Policy Evaluation Cycle'),
-    ('{00000000-0000-0000-0000-000000000002}', 'Software Inventory Cycle'),
-    ('{00000000-0000-0000-0000-000000000031}', 'Software Metering Usage Report Cycle'),
-    ('{00000000-0000-0000-0000-000000000114}', 'Software Updates Deployment Evaluation Cycle'),
-    ('{00000000-0000-0000-0000-000000000113}', 'Software Update Scan Cycle'),
-    ('{00000000-0000-0000-0000-000000000032}', 'Windows Installers Source List Update Cycle')
-)
+# Setup Config Manager hash table to be used to run cycles
+$configMgrCycles = @{
+  'Application Deployment Evaluation Cycle' = '00000000-0000-0000-0000-000000000121' 
+  'Discovery Data Collection Cycle' = '00000000-0000-0000-0000-000000000003' 
+  'File Collection Cycle' = '00000000-0000-0000-0000-000000000010' 
+  'Hardware Inventory Cycle' = '00000000-0000-0000-0000-000000000001' 
+  'Machine Policy Retrieval Cycle' = '00000000-0000-0000-0000-000000000021' 
+  'Machine Policy Evaluation Cycle' = '00000000-0000-0000-0000-000000000022' 
+  'Software Inventory Cycle' = '00000000-0000-0000-0000-000000000002' 
+  'Software Metering Usage Report Cycle' = '00000000-0000-0000-0000-000000000031' 
+  'Software Updates Deployment Evaluation Cycle' = '00000000-0000-0000-0000-000000000114' 
+  'Software Update Scan Cycle' = '00000000-0000-0000-0000-000000000113' 
+  'Windows Installers Source List Update Cycle' = '00000000-0000-0000-0000-000000000032' 
+}
 
-######################################################################
-# The Process Loop
-######################################################################
+# Main loop
+for ($cycleCounter = 1; $cycleCounter -le 500; $cycleCounter++) {
 
-# here we have a for loop that runs on an incrementing timer, it then runs through the cycles in the array piece by piece and outputs all the output to null, with the exception of the names. 
-# It shows what cycle we are on, and clears the screen repeatedly after each, to show motion on the screen and make it easier to see what it is doing. 
-# It likewise checks for a pending reboot and will reboot as it needs to to help with the update process, though it only does this if the CPU usage of the computer is under 10% and network bandwidth is under 1% for half a minute.
-
-#READY, SET, GO!
-
-for ($timer = 1; $timer -le 500; $timer++) {
-
-  # Check for pending reboot
-  # https://www.dbtales.com/using-powershell-check-for-pending-reboot-and-last-restart-on-remote-computer/
-
+  # Check for pending reboots
   $pendingReboot = $false
-  if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { $pendingReboot = $true }
-  if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { $pendingReboot = $true }
-  if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { $pendingReboot = $true }
+  if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) {
+    $pendingReboot = $true
+  }
+  if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) {
+    $pendingReboot = $true
+  }
+  if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) {
+    $pendingReboot = $true
+  }
   try {
     $util = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities"
     $status = $util.DetermineIfRebootPending()
@@ -85,12 +73,13 @@ for ($timer = 1; $timer -le 500; $timer++) {
     }
   }
   catch {}
-
-  # If a reboot is pending and CPU Useage is under 10%, and network bandwidth is under 1% for half a minute, reboot computer
+  
+  # If a pending reboot is detected, verify if the system is idle.
   if ($pendingReboot) {
-    $cpuUseage = Get-CimInstance win32_processor | Measure-Object -Property LoadPercentage -Average
-    if ($cpuUseage.Average -le 10) {
-      # https://ss64.com/ps/syntax-get-bandwidth.html
+
+    # If CPU usage is less than or equal to 10%, check the network bandwidth usage.
+    $cpuUsage = Get-CimInstance win32_processor | Measure-Object -Property LoadPercentage -Average
+    if ($cpuUsage.Average -le 10) {
       $startTime = get-date
       $endTime = $startTime.addMinutes(0.5)
       $timeSpan = new-timespan $startTime $endTime
@@ -99,14 +88,14 @@ for ($timer = 1; $timer -le 500; $timer++) {
       $totalBandwidth = 0
 
       while ($timeSpan -gt 0) {
-        # Get an object for the network interfaces, excluding any that are currently disabled.
-        $colInterfaces = Get-CimInstance -class Win32_PerfFormattedData_Tcpip_NetworkInterface | select BytesTotalPersec, CurrentBandwidth, PacketsPersec | where { $_.PacketsPersec -gt 0 }
+        $colInterfaces = Get-CimInstance -Class Win32_PerfFormattedData_Tcpip_NetworkInterface -ErrorAction SilentlyContinue | 
+        Select-Object -Property BytesTotalPersec, CurrentBandwidth, PacketsPersec | 
+        Where-Object { $_.PacketsPersec -gt 0 }
 
         foreach ($interface in $colInterfaces) {
           $bitsPerSec = $interface.BytesTotalPersec * 8
           $totalBits = $interface.CurrentBandwidth
 
-          # Exclude Nulls (any WMI failures)
           if ($totalBits -gt 0) {
             $result = (( $bitsPerSec / $totalBits) * 100)
             $totalBandwidth = $totalBandwidth + $result
@@ -114,10 +103,10 @@ for ($timer = 1; $timer -le 500; $timer++) {
           }
         }
         Start-Sleep -milliseconds 100
-
-        # recalculate the remaining time
         $timeSpan = new-timespan $(Get-Date) $endTime
       }
+
+      # If the average network bandwidth usage is less than or equal to 1%, force a system reboot and exit the script.
       $averageBandwidth = $totalBandwidth / $count
       if ($averageBandwidth -le 1) {
         Restart-Computer -Force
@@ -126,22 +115,25 @@ for ($timer = 1; $timer -le 500; $timer++) {
     }
   }
 
-  ForEach ($Cycle in $ConfigManCycles) {
-    write-host "Working on" $Cycle[1]
-    Invoke-WMIMethod -ComputerName $env:COMPUTERNAME -Namespace root\ccm -Class SMS_CLIENT -Name TriggerSchedule $Cycle[0] | Out-Null
+  # Run Configuration Manager Cycles to update and synchronize the device with the Configuration Manager.
+  Write-Host "==== Starting Configuration Manager Cycles ====" -ForegroundColor Red -BackgroundColor White
+  ForEach ($Cycle in $configMgrCycles) {
+    write-host "Working on $($Cycle.Name)" -ForegroundColor Green
+    Invoke-WMIMethod -ComputerName $env:COMPUTERNAME -Namespace root\ccm -Class SMS_CLIENT -Name TriggerSchedule $Cycle.Value | Out-Null
   }
 
-  Write-Host "`nWe are on Cycle: $timer"
+  Write-Host "`nWe are on Cycle: $cycleCounter"
   Write-Host "Ctrl+C to return to Powershell, or just close the window." -ForegroundColor Cyan -BackgroundColor Darkcyan
 
-  # Thank you DrakharD
-  # https://gist.github.com/ctigeek/bd637eeaeeb71c5b17f4
+  # Wait for 1200 seconds (20 minutes) before running the next cycle.
   $seconds = 1200
-  $doneDT = (Get-Date).AddSeconds($seconds)
-  while ($doneDT -gt (Get-Date)) {
-    $secondsLeft = $doneDT.Subtract((Get-Date)).TotalSeconds
-    $percent = ($seconds - $secondsLeft) / $seconds * 100
-    Write-Progress -Activity "Waiting for..." -Status "Sleeping" -SecondsRemaining $secondsLeft -PercentComplete $percent
+  $cycleEndTime = (Get-Date).AddSeconds($seconds)
+
+  # Display a progress bar while waiting.
+  while ($cycleEndTime -gt (Get-Date)) {
+    $remainingSeconds = $cycleEndTime.Subtract((Get-Date)).TotalSeconds
+    $percent = ($seconds - $remainingSeconds) / $seconds * 100
+    Write-Progress -Activity "Waiting for..." -Status "Sleeping" -SecondsRemaining $remainingSeconds -PercentComplete $percent
     Start-Sleep -Seconds 1
   }
   Write-Progress -Activity "Sleeping" -Status "Sleeping..." -SecondsRemaining 0 -Completed
