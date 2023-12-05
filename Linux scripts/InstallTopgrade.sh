7739d2820aa9923cd6b831430ca950dd1f05ac2a
Fenix <daystar999964@gmail.com>
2023-12-05 14:09:26 -0700

    Adding Multi-Arch Capability to InstallTopgrade.sh
    Added support for installing to: aarch64

--- Linux scripts/InstallTopgrade.sh
+++ Linux scripts/InstallTopgrade.sh
@@ -6,9 +6,18 @@
 #This is a simple script to install Topgrade, the all-in one upgrade program repository here: https://github.com/topgrade-rs/topgrade
 #Tested on Ubuntu Linux, though should run on most Linuxes, it downloads the latest version, and then copies it into your /bin folder so it's automatically in your path.
 
-#Lets download the latest copy of Topgrade:
+#Lets download the latest copy of Topgrade, we'll change this accordingly to if
 
-curl -sL https://api.github.com/repos/topgrade-rs/topgrade/releases/latest | grep "browser_download_url.*x86_64-unknown-linux-gnu.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -i - -P /tmp
+ARCH=$(arch)
+
+if [ "$ARCH" == "x86_64" ]; then
+    curl -sL https://api.github.com/repos/topgrade-rs/topgrade/releases/latest | grep "browser_download_url.*x86_64-unknown-linux-gnu.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -i - -P /tmp
+elif [ "$ARCH" == "aarch64" ]; then
+    curl -sL https://api.github.com/repos/topgrade-rs/topgrade/releases/latest | grep "browser_download_url.*aarch64-unknown-linux-gnu.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -i - -P /tmp
+else
+    echo "Unsupported architecture: Ending Script here."
+    exit 1
+fi
 
 #Now lets unpack it and get ready to move it.
 
