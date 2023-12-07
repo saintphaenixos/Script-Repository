0bfbdfe26802df21925b40cc552781a2d49d1511
Fenix <daystar999964@gmail.com>
2023-12-06 20:12:53 -0700

    Fixing how programs are executed again:
    They Aren't shell scripts apparently, they are binaries.

--- BSD scripts/NetBSD scripts/MajorPointReleaseUpgradeNetBSD.sh
+++ BSD scripts/NetBSD scripts/MajorPointReleaseUpgradeNetBSD.sh
@@ -15,16 +15,16 @@ fi
 echo "This script is for use to upgrade to major point releases E.g. 10.X or 11.X NetBSD releases"
 echo "This shouldn't be needed between X.1 or X.12 releases, only for major releases."
 
-read -p "What version of NetBSD are we going to? e.g. 9.5, 10.4 or 15.2?" $BSDVer
+read -p "What version of NetBSD are we going to? e.g. 9.5, 10.4 or 15.2?  " $BSDVer
 
 # Now lets run the upgrade preparations:
-sh /usr/pkgsrc/sysutils/sysupgrade fetch https://cdn.NetBSD.org/pub/NetBSD/NetBSD-$BSDVer/amd64
-sh /usr/pkgsrc/sysutils/sysupgrade kernel
-sh /usr/pkgsrc/sysutils/sysupgrade modules
-sh /usr/pkgsrc/sysutils/sysupgrade sets
-sh /usr/pkgsrc/sysutils/sysupgrade etcupdate
-sh /usr/pkgsrc/sysutils/sysupgrade postinstall
-sh /usr/pkgsrc/sysutils/sysupgrade clean
+/usr/pkgsrc/sysutils/sysupgrade fetch https://cdn.NetBSD.org/pub/NetBSD/NetBSD-$BSDVer/amd64
+/usr/pkgsrc/sysutils/sysupgrade kernel
+/usr/pkgsrc/sysutils/sysupgrade modules
+/usr/pkgsrc/sysutils/sysupgrade sets
+/usr/pkgsrc/sysutils/sysupgrade etcupdate
+/usr/pkgsrc/sysutils/sysupgrade postinstall
+/usr/pkgsrc/sysutils/sysupgrade clean
 reboot
 
 echo "Don't forget to run UpgradeNetBSD.sh afterward!"
